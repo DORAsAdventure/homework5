459beda19efb1a8ec9a49bf80ee15c6af1f51c90
DORAsAdventure <168351749+DORAsAdventure@users.noreply.github.com>
2024-05-04 04:20:15 +0800

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+#immutable_var=(1, 2, 'cat', True)
+#print(immutable_var)
+#immutable_var[2]='dog'
+#print(immutable_var) #кортеж не поддерживает обращение по элеентам
+mutable_list=['cat','dog','panda','lamb']
+mutable_list[0]='kitty'
+mutable_list.append('animal')
+print(mutable_list)

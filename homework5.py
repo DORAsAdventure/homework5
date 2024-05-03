cd766d0edb1a92893160cbf39a1f0510c7c0fe00
DORAsAdventure <168351749+DORAsAdventure@users.noreply.github.com>
2024-05-04 04:21:14 +0800

    homework5.py

--- homework5.py
+++ homework5.py
@@ -1,7 +1,7 @@
 #immutable_var=(1, 2, 'cat', True)
 #print(immutable_var)
 #immutable_var[2]='dog'
-#print(immutable_var) #кортеж не поддерживает обращение по элеентам
+#print(immutable_var) #кортеж не поддерживает обращение по элементам
 mutable_list=['cat','dog','panda','lamb']
 mutable_list[0]='kitty'
 mutable_list.append('animal')
